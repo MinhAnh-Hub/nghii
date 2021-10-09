6130306df9f0f5008acea3648af3787e472c2eed
Minh Anh <85324974+MinhAnh-Hub@users.noreply.github.com>
2021-10-10 02:07:01 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'nghii mỡ à!',
+    introDesc: `Đôi khi lỡ hẹn một giờ
+Lần sau muốn gặp phải chờ trăm năm.`,
+    btnIntro: 'hihi',
+    title: 'Em có thể làm công chúa của anh được không 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Dạ, mình yêu nhau anh nhé',
+    btnNo: 'Không, mình nên làm bạn thôi :3',
+    question:'Dù có chuyện gì thì yêu anh, em nhé',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu công chúa nhiều nhiều 😘😘',
+    messDesc: 'Tối nay mình call cho nhau nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.comh/MinAnhSg' 
+}
